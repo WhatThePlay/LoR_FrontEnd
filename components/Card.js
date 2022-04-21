9a109f929fdb42c7e0c96e6f49300a4afff5210a
bdrein <bdrein@bbcag.ch>
2022-04-21 11:17:25 +0200

    All cards are now displayed. Search function added for cards

--- components/Card.js
+++ components/Card.js
@@ -1,13 +1,23 @@
 import Image from "next/Image"
+import styles from "./Card.module.css"
 
-export default function Card({name}){
+export default function Card({card}){
+
+    let imageWidth = 0;
+    if (card.type === "Unit") {
+        imageWidth = 512
+    } else{
+        imageWidth = 256
+    }
 
     return(
-        <div>
-            <Image src="picture" alt="name" width={2048} height={1024} />
-            <h2>{name}</h2>
-            <p>
-                region <br/> type
+        <div className={styles.cardContainer}>
+            <div className={styles.picture}>
+                <Image src={card.picture2} alt={card} width={imageWidth} height={256} quality={75}/>
+            </div>
+            <h2 className={styles.cardName}>{card.name}</h2>
+            <p className={styles.cardInfo}>
+                {card.linkedRegions[0].name} <br/> {card.type}
             </p>
 
         </div>
--- pages/cards/index.js
+++ pages/cards/index.js
@@ -1,21 +1,64 @@
 import Image from "next/Image"
 import styles from "./index.module.css"
+
 import Card from "@components/Card";
+import {useEffect, useState} from "react";
+import {getAllCards} from "@lib/api";
 
 export default function CardsPage(){
+
+    const [cards, setCards] = useState([])
+
+    const [query, setQuery] = useState("")
+    const handleChange = (value) => setQuery(value.target.value)
+
+    const cardsToRender = Object.values(cards).filter(card => card.name.toLowerCase().includes(query.toLowerCase()))
+
+    useEffect(() => {
+        const loadCards = async () => {
+            try {
+                const cards = await getAllCards()
+                setCards(cards)
+            } catch (e) {
+                alert("Could not load cards!")
+            }
+        }
+        loadCards()
+    }, [])
+
     return (
         <div>
 
-            <div className={styles.banner}>
-                <Image alt="banner" src="https://dd.b.pvp.net/1_0_0/set1/en_us/img/cards/01IO014-full.png" width={2048} height={1024} quality={100} />
+            <div className={styles.bannerContainer}>
+                <div className={styles.banner}>
+                    <Image alt="banner" src="https://dd.b.pvp.net/1_0_0/set1/en_us/img/cards/01IO014-full.png" width={2048} height={1024} quality={100} />
+                </div>
             </div>
+
             <h1>
                 All the Cards
             </h1>
             <p>
-                Here you can check out all the existing cards. You can click on them to see them in a more detailed view. There
-                will also be a search function in the future.
+                Here you can check out all the existing cards. You can click on them to see them in a more detailed view. There is now a search function!
             </p>
+
+            <div className={styles.searchBarContainer}>
+                <input className={styles.searchBar} type="text" onChange={handleChange} />
+            </div>
+
+            <section className={styles.cardSection}>
+                {cards && <>
+                    {
+                        cardsToRender.map(card => {
+                            return (
+                                <>
+                                    <Card card={card}/>
+                                </>
+                            )
+                        })
+                    }
+                </>}
+            </section>
         </div>
     )
 }
